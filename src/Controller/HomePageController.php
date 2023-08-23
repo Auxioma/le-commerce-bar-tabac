e600cbca1656c60e9561d45804d7b37e0ab5ead1
Auxioma <auxioma@yandex.fr>
2023-08-23 13:29:55 +0200

    Envoie mail

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -9,7 +9,9 @@ use App\Entity\Contact;
 use App\Form\ContactType;
 use App\Repository\HoraireRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,11 +19,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomePageController extends AbstractController
 {
     private EntityManagerInterface $entityManager;
+    private MailerInterface $mailer;
 
     public function __construct(
         EntityManagerInterface $entityManager,
+        MailerInterface $mailer
     ) {
         $this->entityManager = $entityManager;
+        $this->mailer = $mailer;
     }
 
 
@@ -51,6 +56,9 @@ class HomePageController extends AbstractController
         $formSms = $this->createForm(SmsType::class, $SMS);
         $formSms->handleRequest($request);
 
+        /**
+         * Formulaire du SMS
+         */
         if ($formSms->isSubmitted() && $formSms->isValid()) {
             // je vais formater le numéro de téléphone pour qu'il soit au format E164
             $numero = $formSms->get('telephone')->getData();
@@ -80,6 +88,33 @@ class HomePageController extends AbstractController
                 throw $e;
             }
             $this->addFlash('success', 'Votre message a bien été envoyé');
+            return $this->redirectToRoute('app_home_page');
+        }
+
+        /**
+         * Formulaire de contact
+         */
+        if ($form->isSubmitted() && $form->isValid()){
+            
+            $email = (new TemplatedEmail())
+                ->from($form->get('Email')->getData())
+                ->to('noreply@lecommercebartabac.com')
+                ->subject('Nouveaux message depuis le formulaire de contact')
+                ->htmlTemplate('email/contact.html.twig')
+                ->context(
+                    [
+                        'Nom' => $form->get('Nom')->getData(),
+                        'Email' => $form->get('Email')->getData(),
+                        'Telephone' => $form->get('Telephone')->getData(),
+                        'Message' => $form->get('Message')->getData(),
+                    ]
+                )
+            ;
+
+            $this->mailer->send($email);
+
+            $this->addFlash('success', 'Votre message a bien été envoyé !');
+
             return $this->redirectToRoute('app_home_page');
         }
         
