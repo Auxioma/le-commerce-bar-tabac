6ab419b9aa85e020d9b07d869582bd88a41721f6
Auxioma <auxioma@yandex.fr>
2023-08-24 17:54:43 +0200

    EasyAdmin + Export CSV

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Sms;
+use App\Controller\Admin\SmsCrudController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+
+        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        return $this->redirect($adminUrlGenerator->setController(SmsCrudController::class)->generateUrl());
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Le Commerce Bar Tabac');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Liste des numéro', 'fas fa-phone', Sms::class);
+    }
+}
---
+++ src/Controller/Admin/SmsCrudController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Sms;
+use App\Repository\SmsRepository;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class SmsCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Sms::class;
+    }
+
+    public const EXPORT = 'export';
+
+    public function configureActions(Actions $actions): Actions
+    {
+        $duplicate = Action::new(self::EXPORT)
+            ->linkToCrudAction('exportSms')
+            ->setCssClass('btn btn-info');
+
+        return $actions
+        ->add(Crud::PAGE_DETAIL, $duplicate)
+        ->disable('new', 'delete', 'edit')
+        ->setPermission(Action::NEW, 'ROLE_ADMIN')
+        ;
+    }
+
+    /**
+     * Je vais exporter les données de la table sms
+     */
+    public function exportSms(SmsRepository $smsRepository)
+    {
+        // Je récupère toutes les données de la table sms
+        $sms = $smsRepository->findAll();
+
+        // Je vais créer un fichier csv
+        $file = fopen('export.csv', 'w');
+
+        // Je vais écrire dans le fichier csv
+        foreach ($sms as $sms) {
+            fputcsv($file, [
+                        $sms->getId(), 
+                        $sms->getTelephone(), 
+                        $sms->getNom(), 
+                        $sms->getPrenom()
+                    ]);
+        }
+
+        // Je ferme le fichier csv
+        fclose($file);
+
+        // Je vais télécharger le fichier csv
+        return $this->file('export.csv');
+    }
+}
